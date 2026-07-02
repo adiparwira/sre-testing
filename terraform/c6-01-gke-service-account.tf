55a07d7a18b92d6bcc129462c94fe41cb1207ca6
Aryadi <adiparwira@yahoo.com>
2026-07-03 01:02:36 +0700

    update deployment and add permision

--- terraform/c6-01-gke-service-account.tf
+++ terraform/c6-01-gke-service-account.tf
@@ -2,3 +2,21 @@ resource "google_service_account" "gke_sa" {
   account_id   = "${local.name}-gke-sa"
   display_name = "${local.name} GKE Service Account"
 }
+
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = var.gcp_project
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.gke_sa.email}"
+}
+
+resource "google_project_iam_member" "logging" {
+  project = var.gcp_project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.gke_sa.email}"
+}
+
+resource "google_project_iam_member" "monitoring" {
+  project = var.gcp_project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.gke_sa.email}"
+}
