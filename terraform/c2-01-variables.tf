3e6cbf84aaa04720610107a67c74b07b52e33f13
Aryadi <adiparwira@yahoo.com>
2026-07-02 23:34:04 +0700

    add create gke cluster

---
+++ terraform/c2-01-variables.tf
@@ -0,0 +1,72 @@
+# Input Variables
+# GCP Project
+variable "gcp_project" {
+  description = "Project in which GCP Resources to be created"
+  type = string
+  default = "gcplearn999"
+}
+
+# GCP Region
+variable "gcp_region1" {
+  description = "Region in which GCP Resources to be created"
+  type = string
+  default = "us-central1"
+}
+
+# GCP Compute Engine Machine Type
+variable "machine_type" {
+  description = "Compute Engine Machine Type"
+  type = string
+  default = "n1-standard-1"
+}
+
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "digital"
+}
+
+# CIDR IP Ranges
+variable "subnet_ip_range" {
+  description = "Subnet IP range"
+  type = string
+  default = "10.129.0.0/20"
+}
+
+variable "pods_ip_range" {
+  description = "Kubernetes Pods IP range"
+  type = string
+  default = "10.11.0.0/21"
+}
+
+variable "services_ip_range" {
+  description = "Kubernetes Services IP range"
+  type = string
+  default = "10.12.0.0/21"
+}
+
+variable "master_ip_range" {
+  description = "Kubernetes Master IP range"
+  type = string
+  default = "10.13.0.0/28"
+}
+
+variable "gke_node_locations" {
+  description = "Node locations for regional GKE cluster"
+  type        = list(string)
+
+  default = [
+    "us-central1-a",
+    "us-central1-b",
+    "us-central1-f"
+  ]
+}
---
+++ terraform/c5-datasource.tf
@@ -0,0 +1,13 @@
+# Terraform Datasources
+# Datasource: Get a list of Google Compute zones that are UP in a region
+data "google_compute_zones" "available" { 
+  status = "UP"   
+}
+
+# Output value
+output "compute_zones" {
+  description = "List of compute zones"
+  #value = data.google_compute_zones.available.names
+  value = var.gke_node_locations
+}
+
